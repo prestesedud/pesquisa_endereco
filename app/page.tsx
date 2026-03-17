531df6ff786b19192982045c91528329dd8c389b
David Prestes <davidupts@gmail.com>
2026-03-17 20:15:11 -0300

    primeiro commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,122 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from 'react';
+
+type Endereco = {
+  cep: string;
+  logradouro: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  ibge: string;
+};
 
 export default function Home() {
+  const [cep, setCep] = useState('');
+  const [endereco, setEndereco] = useState<Endereco | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [erro, setErro] = useState('');
+
+  function delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async function buscarCep() {
+    try {
+      if (cep.length !== 8) {
+        setErro('Por favor, insira um CEP válido!');
+        return;
+      }
+
+      setErro('');
+      setLoading(true);
+
+      const startTime = Date.now();
+
+      const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      const data = await response.json();
+
+      const elapsed = Date.now() - startTime;
+      const MIN_TIME = 1000;
+
+      if (elapsed < MIN_TIME) {
+        await new Promise((resolve) => setTimeout(resolve, MIN_TIME - elapsed));
+      }
+
+      if (data.erro) {
+        setErro('CEP não encontrado');
+        setEndereco(null);
+      } else {
+        setEndereco(data);
+      }
+    } catch (error) {
+      setErro('Erro ao buscar o CEP');
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <main className="flex min-h-screen items-center justify-center bg-red-100">
+      <div className="bg-white p-8 rounded-xl shadow-md">
+        <h1 className="text-2xl font-bold text-red-500 mb-6 text-center">
+          Buscador de CEP
+        </h1>
+
+        <input
+          type="text"
+          placeholder="Digite o CEP"
+          value={cep}
+          onChange={(e) => setCep(e.target.value)}
+          className="w-full border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-300 rounded-lg shadow-sm placeholder:text-gray-400 text-gray-400"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+
+        <button
+          disabled={loading}
+          onClick={buscarCep}
+          className="mt-3 w-full bg-amber-400 text-white p-3 rounded-lg hover:bg-amber-300 transition"
+        >
+          {loading ? 'Buscando...' : 'Buscar'}
+        </button>
+
+        {erro && (
+          <p className="mt-4 bg-red-100 border border-red-300 text-red-600 rounded-lg text-sm font-medium p-3">
+            {erro}
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+        )}
+
+        {loading && (
+          <div className="mt-4 flex justify-center">
+            <div className="w-6 h-6 border-4 border-amber-200 border-t-transparent rounded-full animate-spin"></div>
+          </div>
+        )}
+
+        {endereco && (
+          <div className="mt-6 space-y-2 text-red-600">
+            <p>
+              <strong>Rua: </strong> {endereco.logradouro}
+            </p>
+            <p>
+              <strong>Bairro: </strong> {endereco.bairro}
+            </p>
+            <p>
+              <strong>Cidade: </strong> {endereco.localidade}
+            </p>
+            <p>
+              <strong>Estado: </strong> {endereco.uf}
+            </p>
+
+            <a
+              href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(`${endereco.logradouro}, ${endereco.bairro}, ${endereco.localidade} - ${endereco.uf}`)}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-4 inline-block bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transitio"
+            >
+              Google Maps 📍
+            </a>
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
